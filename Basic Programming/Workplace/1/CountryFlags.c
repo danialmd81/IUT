4f1b4de70fe9b7ec70f18217390c453dacf7741d
Danial Mobini <danialmobinidh81@gmail.com>
2024-06-02 22:24:54 +0330

    Workplace 1

---
+++ Basic Programming/Workplace/1/CountryFlags.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+int main ()
+{
+char f;
+scanf("%c",&f);
+if (f=='I'){
+printf(   "\033[;42m                                       ");printf("\33[0m\n");
+printf(   "\033[;42m                                       ");printf("\33[0m\n");
+printf(   "\033[;42m                                       ");printf("\33[0m\n");
+printf(   "\033[;47m                                       ");printf("\33[0m\n");
+printf(   "\033[;47m                                       ");printf("\33[0m\n");
+printf(   "\033[;47m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+}
+
+if (f=='U'){
+printf(   "\033[;44m             ");printf("\033[;41m                          ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf("\033[;47m                          ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf("\033[;41m                          ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf("\033[;47m                          ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;47m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;47m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+}
+if (f=='C'){
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+}
+if (f=='R'){
+printf(   "\033[;47m                                       ");printf("\33[0m\n");
+printf(   "\033[;47m                                       ");printf("\33[0m\n");
+printf(   "\033[;47m                                       ");printf("\33[0m\n");
+printf(   "\033[;44m                                       ");printf("\33[0m\n");
+printf(   "\033[;44m                                       ");printf("\33[0m\n");
+printf(   "\033[;44m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+printf(   "\033[;41m                                       ");printf("\33[0m\n");
+}
+
+if (f=='F'){
+
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+printf(   "\033[;44m             ");printf(   "\033[;47m             ");printf(   "\033[;41m             ");printf("\33[0m\n");
+
+}
+return 0 ;
+}
+
+
+
+
+
+
