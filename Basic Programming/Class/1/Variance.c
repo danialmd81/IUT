a22393a51e4a45c7e083fc5aeec05a01f7e0a1a0
Danial Mobini <danialmobinidh81@gmail.com>
2024-06-03 00:53:04 +0330

    Update

--- Basic Programming/Class/1/Variance.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float a1,a2,a3,a4,a5,j,m,v;
-    scanf("%f%f%f%f%f",&a1,&a2,&a3,&a4,&a5);
-    j=a1+a2+a3+a4+a5;
-    m=j/5;
-    v=(((a1-m)*(a1-m))+((a2-m)*(a2-m))+((a3-m)*(a3-m))+((a4-m)*(a4-m))+((a5-m)*(a5-m)))/5;
-    printf("%.3f\n%.3f\n%.3f",j,m,v);
-}
